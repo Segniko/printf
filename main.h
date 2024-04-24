8bbaf196e0256314407f87f8db5809e9c0109139
Segni <segniassaye2@gmail.com>
2024-04-24 14:37:17 +0000

    Updateg

---
+++ main.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    char c;
+    char *s;
+
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                case 's':
+                    s = va_arg(args, char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+int main() {
+    _printf("Hello, %s! My favorite character is %c. This is a percent sign: %%.\n", "world", 'A');
+    return 0;
+}
